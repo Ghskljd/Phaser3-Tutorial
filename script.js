d31d786ad5eabfe6940772d1879e3e49c1a7399d
Ghskljd JJ <ghskljdjj@Ghskljds-MacBook.local>
2021-02-15 16:44:07 -0800

    Add Player

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function preload() {
 this.load.image('sky', 'assests/sky.png');
 this.load.image('ground', 'assests/platform.png');
 this.load.image('bomb', 'assests/star.png');
-this.load.image('dude', 'assests/dude.png', {frameWidth: 32, frameHeight: 48});
+this.load.spritesheet('dude', 'assests/dude.png', {frameWidth: 32, frameHeight: 48});
 }
 
 let platforms;
@@ -37,6 +37,32 @@ platforms.create(50, 250, 'ground');
 platforms.create(750, 220, 'ground');
 
 this.add.image(400, 300, 'star');
+
+player = this.physics.add.sprite(100, 450, 'dude');
+
+player.setBounce(0.2);
+player.setCollideWorldBounds(true);
+
+this.anims.create({
+	key: 'left',
+	frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
+	frameRate: 10,
+	repeat: -1
+});
+
+this.anims.create({
+	key: 'turn',
+	frames: [{key: 'dude', frame: 4}],
+	frameRate: 20
+});
+
+this.anims.create({
+	key: 'right',
+	frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
+	frameRate: 10,
+	repeat: -1
+});
+
 }
 
 function update(){
