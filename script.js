8d301beac061b54534363cfa77be9b935384f217
Ghskljd JJ <ghskljdjj@Ghskljds-MacBook.local>
2021-03-21 18:03:33 -0700

    Add platforms

--- script.js
+++ script.js
@@ -3,67 +3,45 @@ const config = {
 	width: 800,
 	height: 600,
 	physics: {
-		default: 'arcade'
+		default: 'arcade',
 		arcade: {
 			gravity: {y: 300},
 			debug: false
-			}
-		},
+		}
+	},
 	scene: {
-	   preload: preload,
-	   create: create,
-	   update: update
+		preload: preload,
+		create: create,
+		update: update
 	}
 };
 
 const game = new Phaser.Game(config);
 
-function preload() {
-this.load.image('sky', 'assests/sky.png');
-this.load.image('ground', 'assests/platform.png');
-this.load.image('bomb', 'assests/star.png');
-this.load.spritesheet('dude', 'assests/dude.png', {frameWidth: 32, frameHeight: 48});
+function preload()
+{
+this.load.image('sky', 'assets/sky.png');
+this.load.image('ground', 'assets/platform.png');
+this.load.image('star', 'assets/star.png');
+this.load.image('bomb', 'assets/bomb.png');
+this.load.spritesheet('dude', 'assets/dude.png', {frameWidth: 32, frameHeight: 48});
 }
 
 let platforms;
 
-function create() {
+function create()
+{
 this.add.image(400, 300, 'sky');
 
 platforms = this.physics.add.staticGroup();
+
 platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-platforms.create(600, 400, 'ground'):
+
+platforms.create(600, 400, 'ground');
 platforms.create(50, 250, 'ground');
 platforms.create(750, 220, 'ground');
-
-this.add.image(400, 300, 'star');
-
-player = this.physics.add.sprite(100, 450, 'dude');
-
-player.setBounce(0.2);
-player.setCollideWorldBounds(true);
-
-this.anims.create({
-	key: 'left',
-	frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
-	frameRate: 10,
-	repeat: -1
-});
-
-this.anims.create({
-	key: 'turn',
-	frames: [{key: 'dude', frame: 4}],
-	frameRate: 20
-});
-
-this.anims.create({
-	key: 'right',
-	frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
-	frameRate: 10,
-	repeat: -1
-});
-
 }
 
-function update(){
+function update()
+{
 }
